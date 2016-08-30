188ac528966a74c2d112a111b89b5621c9999488
Tyler Petresky <tnpetresky@gmail.com>
2016-08-30 16:09:30 -0400

    updated code to output nicely

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var url     = require('url');
 
-// exports.handler = function(event, context, callback) {
 function scrape(event, context, callback) {
-  console.log('Scraping');
+  console.log('Scraping parking.ucf.edu...');
   var host_name = 'http://secure.parking.ucf.edu/GarageCount/iframe.aspx';
 
   request(host_name, function(error, response, html) {
@@ -14,28 +13,23 @@ function scrape(event, context, callback) {
     }
 
     var $ = cheerio.load(html);
-
     var data = {};
 
     var rows = $('.dxgvDataRow_DevEx');
     for (var i = 0; i < rows.length; i += 1) {
       var name = rows[i].children[1].children[0].data;
-      var free = rows[i].children[2].children[0].next.children[0].data;
-      var total = rows[i].children[2].children[0].next.next.data;
-      data[name] = {
-        free: parseInt(free),
-        total: parseInt(total.slice(1).trim())
-      };
+      data[name] = {};
+      data[name].free = parseInt(rows[i].children[2].children[0].next.children[0].data);
+      data[name].total = parseInt(rows[i].children[2].children[0].next.next.data.slice(1).trim());
       data[name].percentage = (data[name].total - data[name].free) / data[name].total;
+
+      console.log('Reading ' + name + ': ' + data[name].free + '/' + data[name].total);
     }
 
+    console.log("----------------------------");
     console.log(data);
 
   });
 }
 
 scrape();
-
-
-
-// exports = module.exports = app;
